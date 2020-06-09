e54b8a792fc203107586a46bbacd74d6f3898d7d
AnikaZN <anikanacey@gmail.com>
2020-06-08 20:32:16 -0600

    Factorial exercise

---
+++ factorial.py
@@ -0,0 +1,35 @@
+# GOAL: Create a factorial function
+
+# Understand:
+# - What is a factorial?
+# -- n! / n * (n - 1)...1
+
+# Plan:
+# - Recursively or iteratively?
+# -- Recursion, since this is an unpredictably repeating problem
+# --- Base case (n = 1), Move towards it (multiply by n - 1?), Call itself
+#    (function_name(result))
+
+# Execute:
+
+def factorial_rec(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial_rec(n - 1)
+
+# Plan
+# - Let's try it iteratively!
+# -- Make a tracker: total, make a while loop to decrement n, multiply tracker
+#    at every step, return tracker
+
+# Execute:
+
+def factorial_it(n):
+    total = 1
+
+    while n != 1:
+        total *= n
+        n -= 1
+
+    return total
