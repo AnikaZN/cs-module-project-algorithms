13983e712d330ce9f06e23bad39cebcdd251bd55
AnikaZN <anikanacey@gmail.com>
2020-06-15 20:30:26 -0600

    Update to eating_cookies and moving_zeroes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,7 @@ def eating_cookies(n):
     # Your code here
 
     if n <= 0:
-        return "Oh no! There's no cookies in the cookie jar!"
+        return 1
 
     if n == 1:
         return 1
@@ -14,16 +14,16 @@ def eating_cookies(n):
     if n == 2:
         return 2
 
-    # Can always eat the cookies one at a time
-    ways = 1
+    if n > 2:
+        sequence = [1, 1, 2]
+        for x in range(n - 2):
+            sequence.append(sum(sequence))
+            sequence.pop(0)
 
-    # Use // division
-
-
-    pass
+        return sequence[2]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,19 +4,19 @@ Returns: a List of integers
 '''
 from collections import deque
 
-# def moving_zeroes(arr):
-#     # Your code here
-#     new_arr = []
-#     for item in arr:
-#         if item != 0:
-#             new_arr.insert(0, item) # O(n) - goal is O(1)
-#         else:
-#             new_arr.append(item)
-#
-#     return new_arr
-
-
 def moving_zeroes(arr):
+    # Your code here
+    new_arr = []
+    for item in arr:
+        if item != 0:
+            new_arr.insert(0, item) # O(n) - goal is O(1)
+        else:
+            new_arr.append(item)
+
+    return new_arr
+
+
+def moving_zeroes_advanced(arr):
     # Your code here
     new_arr = deque()
     for item in arr:
